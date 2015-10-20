d3c77542b047cf55a931b204881a9b1fe4eada83
Noman Khan <nomaanhaikhan@hotmail.com>
2015-10-20 16:17:43 -0700

    Wrote the current_instances method

--- cmpe273_aws_dashboard.py
+++ cmpe273_aws_dashboard.py
@@ -15,5 +15,26 @@ class AWSMetrics:
 	else:
 	    print "API Key and API Secret is missing - Kindly Check !!!"
 	    return None
+	    
+	    
+    def get_current_instances(self):
+	try:
+	    conn = self.get_ec2_conn()
+	    reservations = conn.get_all_instances()
+	    instances = [i for r in reservations for i in r.instances]
+	    for i in instances:
+		print "InstanceId:%s, Instance Type: %s, Launch Time: %s, Ip Address: %s, Public DNS Name: %s" %(i.id, i.instance_type, i.launch_time, i.ip_address, i.public_dns_name)
+	except Exception, e:
+	    print e
+	    
+	    
 
+	    
+if __name__ == "__main__":
+    ACCESS_KEY='AKIAJ4H7QY35TCYBPIRA'
+    SECRET='izxv5KO6O8hazTDeGZV8gUUarWNsXF9UUo3DGEIR'
+    aws_object = AWSMetrics(ACCESS_KEY, SECRET)
+    print "Gettings all the servers which are present in your account "
+    aws_object.get_current_instances()
+   
   
